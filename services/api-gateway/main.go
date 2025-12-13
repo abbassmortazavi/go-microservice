c308244e83e3cf2cdce7bc5cd64540633055ed08
Abbass Mortazavi <abbass.mortazavi@patoughi.info>
2025-12-13 10:23:28 +0330

    feat: add kuber config for api gateway

--- services/api-gateway/main.go
+++ services/api-gateway/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "log"
+import (
+	"abbassmortazavi/go-microservice/pkg/env"
+	"log"
+	"net/http"
+)
+
+var (
+	httpAddr = env.GetString("GATEWAY_HTTP_ADDR", ":8080")
+)
 
 func main() {
 	log.Println("Starting API Gateway")
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /test-url", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Preview called")
+	})
 }
