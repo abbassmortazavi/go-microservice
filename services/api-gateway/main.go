27136862d2c37b9cd0bb9768900e48c9d91ec3a8
Abbass Mortazavi <abbass.mortazavi@patoughi.info>
2025-12-13 10:36:52 +0330

    feat: add go routin to run server

--- services/api-gateway/main.go
+++ services/api-gateway/main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"abbassmortazavi/go-microservice/pkg/env"
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 var (
@@ -16,4 +21,29 @@ func main() {
 	mux.HandleFunc("POST /test-url", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Preview called")
 	})
+
+	server := &http.Server{
+		Addr:    httpAddr,
+		Handler: mux,
+	}
+	serverErrors := make(chan error, 1)
+	go func() {
+		log.Println("Listening on ", httpAddr)
+		serverErrors <- server.ListenAndServe()
+	}()
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+
+	select {
+	case err := <-serverErrors:
+		log.Fatalf("Error starting server: %v", err)
+	case sig := <-shutdown:
+		log.Println("Shutting down server...", sig)
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := server.Shutdown(ctx); err != nil {
+			log.Fatalf("Error shutting down server: %v", err)
+			server.Close()
+		}
+	}
 }
