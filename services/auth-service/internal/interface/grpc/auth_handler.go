bfe80db581cdf8076bb47f45b2875ff7d5307f32
Abbass Mortazavi <abbass.mortazavi@patoughi.info>
2025-12-17 22:27:58 +0330

    feat: complete register handler

--- services/auth-service/internal/interface/grpc/auth_handler.go
+++ services/auth-service/internal/interface/grpc/auth_handler.go
@@ -1,1 +1,28 @@
 package grpc
+
+import (
+	authpb "abbassmortazavi/go-microservice/pkg/proto/auth"
+	"abbassmortazavi/go-microservice/services/auth-service/internal/usecase"
+	"context"
+)
+
+type AuthHandler struct {
+	authpb.UnimplementedAuthServiceServer
+	usecase *usecase.AuthUseCase
+}
+
+func NewAuthHandler(u *usecase.AuthUseCase) *AuthHandler {
+	return &AuthHandler{
+		usecase: u,
+	}
+}
+
+func (h *AuthHandler) Register(ctx context.Context, req *authpb.RegisterRequest) (*authpb.RegisterResponse, error) {
+	// password hashing later
+	err := h.usecase.Register(ctx, req.Email, req.Password, req.Name)
+	if err != nil {
+		return nil, err
+	}
+
+	return &authpb.RegisterResponse{Message: "registered"}, nil
+}
---
+++ services/auth-service/migrations/001_create_users.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users
+(
+    id         SERIAL PRIMARY KEY,
+    name       VARCHAR(50)         NOT NULL,
+    email      VARCHAR(100) UNIQUE NOT NULL,
+    role       VARCHAR(100)        NOT NULL,
+    password   VARCHAR(255)        NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
