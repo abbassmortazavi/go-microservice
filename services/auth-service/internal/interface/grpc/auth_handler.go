4fac906422fb2185b8abb1b6c98251f38466b9d6
Abbass Mortazavi <abbass.mortazavi@patoughi.info>
2025-12-18 15:11:49 +0330

    feat: create validate token handler

--- services/auth-service/internal/interface/grpc/auth_handler.go
+++ services/auth-service/internal/interface/grpc/auth_handler.go
@@ -2,27 +2,47 @@ package grpc
 
 import (
 	authpb "abbassmortazavi/go-microservice/pkg/proto/auth"
-	"abbassmortazavi/go-microservice/services/auth-service/internal/usecase"
+	"abbassmortazavi/go-microservice/services/auth-service/internal/domain/service"
 	"context"
 )
 
 type AuthHandler struct {
 	authpb.UnimplementedAuthServiceServer
-	usecase *usecase.AuthUseCase
+	authService *service.AuthService
 }
 
-func NewAuthHandler(u *usecase.AuthUseCase) *AuthHandler {
+func NewAuthHandler(a *service.AuthService) *AuthHandler {
 	return &AuthHandler{
-		usecase: u,
+		authService: a,
 	}
 }
 
 func (h *AuthHandler) Register(ctx context.Context, req *authpb.RegisterRequest) (*authpb.RegisterResponse, error) {
 	// password hashing later
-	err := h.usecase.Register(ctx, req.Email, req.Password, req.Name)
+	err := h.authService.Register(ctx, req.Email, req.Password, req.Name)
 	if err != nil {
 		return nil, err
 	}
 
 	return &authpb.RegisterResponse{Message: "registered"}, nil
+}
+
+func (h *AuthHandler) Login(ctx context.Context, req *authpb.LoginRequest) (*authpb.LoginResponse, error) {
+	access, refresh, err := h.authService.Login(ctx, req.Email, req.Password)
+	if err != nil {
+		return nil, err
+	}
+	return &authpb.LoginResponse{
+		AccessToken:  access,
+		RefreshToken: refresh,
+	}, nil
+}
+func (h *AuthHandler) ValidateToken(ctx context.Context, req *authpb.ValidateTokenRequest) (*authpb.ValidateTokenResponse, error) {
+	claims, err := h.authService.TokenService.ValidateToken(req.Token)
+	if err != nil {
+		return nil, err
+	}
+	return &authpb.ValidateTokenResponse{
+		UserId: claims.Subject,
+	}, nil
 }
