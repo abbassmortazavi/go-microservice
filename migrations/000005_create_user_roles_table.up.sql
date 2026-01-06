13d3ef9e65ffe12cf2dd4bcdfe24678099951aeb
Abbass Mortazavi <abbass.mortazavi@patoughi.info>
2026-01-06 10:14:58 +0330

    feat: bug fix migrations

--- migrations/000005_create_user_roles_table.up.sql
+++ migrations/000005_create_user_roles_table.up.sql
@@ -3,10 +3,10 @@ CREATE TABLE user_roles
     role_id INT NOT NULL,
     user_id INT NOT NULL,
     PRIMARY KEY (role_id, user_id),
-    CONSTRAINT fk_user_roles_role
+    CONSTRAINT fk_user_roles_roles
         FOREIGN KEY (role_id)
-            REFERENCES roles(role_id),
-    CONSTRAINT fk_user_roles_user
+            REFERENCES roles(id),
+    CONSTRAINT fk_user_roles_users
         FOREIGN KEY (user_id)
-            REFERENCES users(user_id)
+            REFERENCES users(id)
 );
--- migrations/000006_create_role_permissions_table.up.sql
+++ migrations/000006_create_role_permissions_table.up.sql
@@ -3,10 +3,10 @@ CREATE TABLE role_permissions
     role_id       INT NOT NULL,
     permission_id INT NOT NULL,
     PRIMARY KEY (role_id, permission_id),
-    CONSTRAINT fk_role_permissions_role
+    CONSTRAINT fk_role_permissions_roles
         FOREIGN KEY (role_id)
-            REFERENCES roles (role_id),
-    CONSTRAINT fk_role_permissions_permission
+            REFERENCES roles (id),
+    CONSTRAINT fk_role_permissions_permissions
         FOREIGN KEY (permission_id)
-            REFERENCES users (permission_id)
+            REFERENCES users (id)
 );
