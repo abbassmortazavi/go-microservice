af5453df13acb45c37a7c0368786cf41883a6161
abbasmortazavi <abbassmortazavi@gmail.com>
2026-06-14 12:46:16 +0330

    fix: implement body in main

--- services/notification-service/cmd/main.go
+++ services/notification-service/cmd/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"abbassmortazavi/go-microservice/services/notification-service/config"
+	"abbassmortazavi/go-microservice/services/notification-service/worker"
+	"log"
+	"os"
+	"os/signal"
+)
+
+func main() {
+	cfg := config.Load()
+	emailWrrker := worker.NewEmailWorker(cfg)
+	go emailWrrker.Start()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	log.Println("Shutting down server...")
+}
